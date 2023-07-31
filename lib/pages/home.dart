4f6d5bf8621ab5df056045a788ac542b21337976
smalhadid <smalhadid@gmail.com>
2023-07-31 16:30:46 -0400

    fixed button design and added navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -8,13 +10,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Pet Home Page'),
+      home: const MyHomePage(title: 'Pet Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+enum TabItem {
+  home,
+  report,
+  map,
+  lostFound,
+  account,
+}
+
+class MyHomePage extends StatefulWidget {
   final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TabItem _currentTab = TabItem.home;
+
   final List<String> buttonLabels = [
     'REPORT',
     'MAP',
@@ -33,63 +53,110 @@ class MyHomePage extends StatelessWidget {
     Icons.home,
   ];
 
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  void _selectTab(int index) {
+    setState(() {
+      _currentTab = TabItem.values[index];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
-      body: Row(
-        children: List.generate(2, (col) {
-          return Expanded(
-            child: Column(
-              children: List.generate(3, (row) {
-                int index = col * 3 + row;
-                return Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      print('Button $index pressed');
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.all(20.0),
-                          decoration: BoxDecoration(
-                            color: Colors.purple,
-                            shape: BoxShape.circle,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle,
-                              ),
-                              child: Icon(
-                                buttonIcons[index],
-                                color: Colors.purple,
-                                size: 50,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.purple.shade100,
+              Colors.white,
+            ],
+          ),
+        ),
+        child: Row(
+          children: List.generate(2, (col) {
+            return Expanded(
+              child: Column(
+                children: List.generate(3, (row) {
+                  int index = col * 3 + row;
+                  return Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        print('Button $index pressed');
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 120.0,
+                            width: 120.0,
+                            margin: const EdgeInsets.all(20.0),
+                            decoration: const BoxDecoration(
+                              color: Colors.purple,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Icon(
+                                  buttonIcons[index],
+                                  color: Colors.purple,
+                                  size: 60,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Text(
-                          buttonLabels[index],
-                          style: TextStyle(
-                            color: Colors.black,
+                          Text(
+                            buttonLabels[index],
+                            style: const TextStyle(
+                              color: Colors.black,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }),
-            ),
-          );
-        }),
+                  );
+                }),
+              ),
+            );
+          }),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.report),
+            label: 'Report',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Lost & Found',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _currentTab.index,
+        selectedItemColor: Colors.purple,
+        unselectedItemColor: Colors.purple,
+        showUnselectedLabels: true,
+        onTap: _selectTab,
       ),
     );
   }
